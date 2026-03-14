5e1e3ffbb27fdd727f53b2f1bc363fd848b7520b
Surpriseofgod1509 <isholaqudus1509@gmail.com>
2026-03-14 15:56:14 -0400

    Update script.js

--- lab4/test2/script.js
+++ lab4/test2/script.js
@@ -5,11 +5,11 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 const image = [
-  { filenames: "pic1.jpg", alt: "Closeup of a human eye" },
-  { filename: "pic2.jpg", alt: "A wave" },
-  { filename: "pic3.jpg", alt: "Purple" },
-  { filename: "pic4.jpg", alt: "Section of wall" },
-  { filename: "pic5.jpg", alt: "Leaf" },
+  { filenames: pic1.jpg, alt: "Closeup of a human eye" },
+  { filename: pic2.jpg, alt: "A wave" },
+  { filename: pic3.jpg, alt: "Purple" },
+  { filename: pic4.jpg, alt: "Section of wall" },
+  { filename: pic5.jpg, alt: "Leaf" },
 ];
 
 const baseURL =
