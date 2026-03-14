01ffb78a8d189234bd2296863593d84c436ae90f
Surpriseofgod1509 <isholaqudus1509@gmail.com>
2026-03-14 16:01:50 -0400

    Update script.js
    i start all over

--- lab4/test2/script.js
+++ lab4/test2/script.js
@@ -3,14 +3,3 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
-
-const image = [
-  { filenames: pic1.jpg, alt: "Closeup of a human eye" },
-  { filename: pic2.jpg, alt: "A wave" },
-  { filename: pic3.jpg, alt: "Purple" },
-  { filename: pic4.jpg, alt: "Section of wall" },
-  { filename: pic5.jpg, alt: "Leaf" },
-];
-
-const baseURL =
-  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
