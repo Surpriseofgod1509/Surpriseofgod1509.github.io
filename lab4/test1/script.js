d9574019d5ba0809d147ad6f987ee9acad14599f
Surpriseofgod1509 <isholaqudus1509@gmail.com>
2026-03-14 12:30:10 -0400

    Update script.js
    Starting again

--- lab4/test1/script.js
+++ lab4/test1/script.js
@@ -10,26 +10,26 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
-const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const places = ["the soup kitchen", "Disneyland", "the White House"];
-const events = [
-  "spontaneously combusted",
-  "melted into a puddle on the sidewalk",
-  "turned into a slug and slithered away",
-];
 
+// Willy the Goblin
+// Big Daddy
+// Father Christmas
+
+// the soup kitchen
+// Disneyland
+// the White House
+
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and slithered away
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  const randomCharacter = randomValueFromArray(characters);
-  const randomPlace = randomValueFromArray(places);
-  const randomEvent = randomValueFromArray(events);
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
-  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
   return storyText;
 }
-
 
 // Event listener and partial generate function definition
 
@@ -41,15 +41,9 @@ function generateStory() {
   }
 
   if (document.getElementById("uk").checked) {
-    
-
-    const weight = Math.round(300 / 14) + " stone";
-    const temperature = Math.round((94 - 32) * (5 / 9)) + " Celsius";
-
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 Fahrenheit", temperature);
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
   }
-
 
   // TODO: replace "" with the correct expression
   story.textContent = "";
