2ec86906dcae1b2a0f98ed2336d3294faa3a74ff
Surpriseofgod1509 <isholaqudus1509@gmail.com>
2026-03-15 14:48:00 -0400

    Update script.js
    correction

--- lab4/test3/script.js
+++ lab4/test3/script.js
@@ -11,6 +11,8 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
+
+// 1. The fully assembled Ball class
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -20,46 +22,24 @@ class Ball {
     this.color = color;
     this.size = size;
   }
-}
-class Ball {
-  // …
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-}
-class Ball {
-  // …
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-}
-const testBall = new Ball(50, 100, 4, 4, "blue", 10);
-testBall.x;
-testBall.size;
-testBall.color;
-testBall.draw();
 
-class Ball {
-  // …
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
   update() {
     if (this.x + this.size >= width) {
       this.velX = -this.velX;
     }
-
     if (this.x - this.size <= 0) {
       this.velX = -this.velX;
     }
-
     if (this.y + this.size >= height) {
       this.velY = -this.velY;
     }
-
     if (this.y - this.size <= 0) {
       this.velY = -this.velY;
     }
@@ -67,4 +47,52 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+
+// 2. Populate the balls array
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+// 3. The main animation loop
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect(); // Check for collisions every frame!
+  }
+
+  requestAnimationFrame(loop);
+}
+
+// Start the animation
+loop();
