b82466a76832263818abd31bfc4feca5675f8ee1
Surpriseofgod1509 <isholaqudus1509@gmail.com>
2026-03-15 16:21:57 -0400

    Update main.js
    Array to store the balls

--- lab4/test4/main.js
+++ lab4/test4/main.js
@@ -138,3 +138,22 @@ class EvilCircle extends Shape {
     }
   }
 }
+
+const balls = [];
+
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count; 
+}
